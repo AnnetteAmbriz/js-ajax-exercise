daa9dec8e60ff2c1b92718c4ed0959db256cbe2b
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-14 13:12:29 -0700

    Ajax harder

---
+++ ajax-exercise.js
@@ -0,0 +1,54 @@
+"use strict";
+
+
+// PART 1: SHOW A FORTUNE
+
+function showFortune(evt) {
+
+    // TODO: get the fortune and show it in the #fortune-text div
+    evt.preventDefault();
+
+    $.get("/fortune", displayFortune);
+}
+
+function displayFortune(result) {
+   $("#fortune-text").html(result);
+
+}
+
+$('#get-fortune-button').on('click', showFortune);
+
+// PART 2: SHOW WEATHER
+
+function showWeather(evt) {
+    evt.preventDefault();
+
+    let url = "/weather.json";
+    let formData = {"zipcode": $("#zipcode-field").val()};
+
+
+    // TODO: request weather with that URL and show the forecast in #weather-info
+    $.get("/weather.json", formData, displayWeather);
+
+}
+
+function displayWeather(result) {
+    console.log(result);
+
+    $("#weather-info").html(result["forecast"] + " " + result["temp"]); //{'forecast': 'Rainy, damp, and rich with hipsters.', 'temp': '60F'};
+}
+
+$("#weather-form").on('submit', showWeather);
+
+// PART 3: ORDER MELONS
+
+function orderMelons(evt) {
+    evt.preventDefault();
+
+    // TODO: show the result message after your form
+    // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+}
+
+$("#order-form").on('submit', orderMelons);
+
+
